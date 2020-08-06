41a0dd2ae44ffb7fac74ce1f2034a5c86cba141a
Arakene <kbh97102@naver.com>
2020-08-06 21:52:06 +0900

    Add QrScanner

--- app/src/main/java/com/fveye/qr/QrScanner.kt
+++ app/src/main/java/com/fveye/qr/QrScanner.kt
@@ -1,4 +1,27 @@
 package com.fveye.qr
 
-class QrScanner {
+import android.content.Context
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.util.*
+
+class QrScanner(val context: Context) {
+
+    private lateinit var barcodeDetector: BarcodeDetector
+
+    private fun initDetector(){
+        barcodeDetector = BarcodeDetector.Builder(context)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build()
+
+    }
+
+    fun detect(frame : Frame) : Barcode{
+        if(!barcodeDetector.isOperational || Objects.isNull(barcodeDetector)){
+            initDetector()
+        }
+        val barcode = barcodeDetector.detect(frame)
+        return barcode.valueAt(0)
+    }
 }
