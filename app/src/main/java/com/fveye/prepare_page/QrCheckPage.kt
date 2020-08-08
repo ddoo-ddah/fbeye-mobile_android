f00696f7729922152c5a6fdf9a8a4767b11713c4
Arakene <kbh97102@naver.com>
2020-08-08 17:29:31 +0900

    Add QrCheckPage

---
+++ app/src/main/java/com/fveye/prepare_page/QrCheckPage.kt
@@ -0,0 +1,15 @@
+package com.fveye.prepare_page
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fveye.R
+
+class QrCheckPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.qr_check_layout)
+    }
+
+
+}
