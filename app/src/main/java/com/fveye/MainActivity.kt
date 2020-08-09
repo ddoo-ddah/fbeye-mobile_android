d1c948ed1be08f820516696f885ca2abd08e1209
Arakene <kbh97102@naver.com>
2020-08-09 10:31:32 +0900

    Update Main

--- app/src/main/java/com/fveye/MainActivity.kt
+++ app/src/main/java/com/fveye/MainActivity.kt
@@ -2,6 +2,8 @@ package com.fveye
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +18,28 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.File
 
-//TODO QR Scanner 붙이기
+//TODO 다음주 할 일
+/* *
+ * 핸드폰은 조작을 하면 안됨 -- pc도 조작해야되는데 유저가 너무 힘듬
+ * 다음화면으로 넘어가는거 자동화 처리
+ * 전부 자동화로 넘어가야됨
+ * 버튼이 있으면 안됨
+ * 코루틴 안되면 그냥 thread
+ * 스샷을 주기적으로 찍으면서 완료가 되면 다음 화면으로 자동으로 넘어가게끔 --> 동영상을 찍으면서 확인하는 거랑 비슷함
+ * 가격 확인
+ * 카메라 안정성 확보
+ * 시험 시작 후 pip 검은화면 -- google 내부적 pip 쓰면 안됨 너무 작게나옴 youtube pip 사이즈 나옴 -- 확인 해보기 like 블루 라이트 필터
+ * ------------------------ 1순위 자동화 ------------------------
+ * ------------------------ 2순위 pip --------------------------
+*/
+
+/**
+ * EyeTracking 은 벡터 두개로 넘어 올거임
+ */
+
+/**
+ * 우선 계속 스크린샷 찍으면서 qr 일정주기로 판단하는 거부터 해보자
+ */
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +51,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var client: CoroutineClient
     private lateinit var snapshotor: Snapshotor
-    private lateinit var outputDir : File
+    private lateinit var outputDir: File
+    private lateinit var backGroundThread: HandlerThread
+    private lateinit var backGroundHandler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,12 +75,25 @@ class MainActivity : AppCompatActivity() {
         send_button.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch { client.write(user_input_editText.text.toString()) }
         }
+
         snapshot_btn.setOnClickListener {
-            Thread {
-                snapshotor.takePhoto()
-                qrScanner.detect()
-            }.start()
+            snapshotWithDelay()
         }
+    }
+
+    private fun snapshot() {
+        snapshotor.takePhoto()
+        snapshotWithDelay()
+    }
+
+    private fun snapshotWithDelay() {
+        backGroundHandler.postDelayed(this::snapshot, 2000)
+    }
+
+    private fun startBackgroundHandler() {
+        backGroundThread = HandlerThread("Back")
+        backGroundThread.start()
+        backGroundHandler = Handler(backGroundThread.looper)
     }
 
     private fun checkPermissionIsGranted() = PERMISSIONS.all {
@@ -81,8 +119,8 @@ class MainActivity : AppCompatActivity() {
                 alertBuilder.apply {
                     setTitle("Permissions are rejected")
                     setMessage("Please Allow Permissions")
-                    setPositiveButton("Yes"){
-                        _,_ ->  ActivityCompat.requestPermissions(this@MainActivity, PERMISSIONS, REQUEST_CODE)
+                    setPositiveButton("Yes") { _, _ ->
+                        ActivityCompat.requestPermissions(this@MainActivity, PERMISSIONS, REQUEST_CODE)
                     }
                 }.create().apply {
                     setCanceledOnTouchOutside(false)
@@ -91,13 +129,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    override fun onStart() {
+        super.onStart()
+        startBackgroundHandler()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         client.disconnect()
         val photoFile = File(
                 outputDir,
-                "test"+".jpg")
-        if(photoFile.exists()){
+                "test" + ".jpg")
+        if (photoFile.exists()) {
             photoFile.delete()
         }
     }
