cdd6ed1c2f1f06b86b13746b36e79a547d1aebf2
Arakene <kbh97102@naver.com>
2020-08-20 15:43:59 +0900

    Update FaceChecker to move examPage

--- app/src/main/java/com/fveye/pages/FaceChecker.kt
+++ app/src/main/java/com/fveye/pages/FaceChecker.kt
@@ -1,13 +1,29 @@
 package com.fveye.pages
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fveye.R
+import com.fveye.network.CoroutineClient
 
 class FaceChecker : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.face_checker_layout)
+
+        waitingForStart()
+    }
+
+    private fun waitingForStart(){
+        Thread{
+            while(true){
+                if (CoroutineClient.getInstance().getAnswer() == "ok"){
+                    break
+                }
+            }
+            val intent = Intent(this, ExamPage::class.java)
+            startActivity(intent)
+        }.start()
     }
 }
