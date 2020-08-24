52f4c3ef70ebca639d701b1d40377870d240a6e5
Arakene <kbh97102@naver.com>
2020-08-24 09:08:38 +0900

    Update Splash delay

--- app/src/main/java/com/fveye/Splash.kt
+++ app/src/main/java/com/fveye/Splash.kt
@@ -3,14 +3,22 @@ package com.fveye
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val intent = Intent(this, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
-        startActivity(intent)
-        finish()
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000)
+            intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+            startActivity(intent)
+            finish()
+        }
     }
 }
