d012155ac24415fa7a32934d5524c0faf13f75da
Arakene <kbh97102@naver.com>
2020-08-28 21:36:23 +0900

    Update ImageClient lateinit to null

--- app/src/main/java/com/fveye/network/ImageClient.kt
+++ app/src/main/java/com/fveye/network/ImageClient.kt
@@ -1,28 +1,24 @@
 package com.fveye.network
 
-import android.graphics.*
-import android.media.Image
+import android.graphics.Bitmap
 import android.os.Build
 import android.util.Base64
-import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.camera.core.ImageProxy
 import io.socket.client.IO
-import io.socket.emitter.Emitter
 import java.io.ByteArrayOutputStream
 import java.util.*
 import java.util.concurrent.Executors
 
 class ImageClient {
 
-    private lateinit var client: io.socket.client.Socket
+    private var client: io.socket.client.Socket? = null
     private val executor = Executors.newFixedThreadPool(3)
 
 
     fun startClient(){
         executor.submit{
             client = IO.socket("http://fbeye.sysbot32.com:3000")
-            client.connect()
+            client!!.connect()
         }
     }
 
@@ -31,21 +27,27 @@ class ImageClient {
         if(Objects.isNull(data)){
             return
         }
+        if(Objects.isNull(client)){
+            startClient()
+            return
+        }
         executor.submit {
             val resizedBitmap = Bitmap.createScaledBitmap(data!!, data.width/4, data.height/4, false)
             val byteArrayOutputStream = ByteArrayOutputStream()
             resizedBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
 
             val base64Data = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT)
-            client.emit("EYE", base64Data)
+            client!!.emit("EYE", base64Data)
         }
     }
 
     fun destroy(){
-        Thread{
-            client.disconnect()
-            client.close()
-        }.start()
+        if (!Objects.isNull(client)){
+            Thread{
+                client!!.disconnect()
+                client!!.close()
+            }.start()
+        }
         executor.shutdownNow()
     }
 
